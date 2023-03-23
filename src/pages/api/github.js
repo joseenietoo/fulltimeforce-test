c52395b373443252d23ab108006cc3158adf40cc
José Nieto <josee.nietoo@gmail.com>
2023-03-23 18:03:22 -0300

    FEAT: add bearer token to GitHub API call

--- src/pages/api/github.js
+++ src/pages/api/github.js
@@ -2,7 +2,12 @@ export default async (req, res) => {
     const repo = "fulltimeforce-test"
     const owner = "joseenietoo"
     const url = "https://api.github.com/repos/"+owner+"/"+repo+"/commits"
-    const response = await fetch(url)
+    const response = await fetch(url, {
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
+        }
+      });
     const jsonResponse = await response.json()
 
     return res.status(200).json({
